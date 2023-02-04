8c39dbfd71d76f36b59e2a2702e4d4cda4503369
Thomas Stassin <thomas.stassin@gsmproductions.com>
2023-02-04 01:16:06 +0100

    end of first day

--- source/game.lua
+++ source/game.lua
@@ -4,22 +4,23 @@ import "CoreLibs/string"
 local gfx <const> = playdate.graphics
 local pd <const> = playdate
 
+local mapSize = { 11, 6 }
+local mapDecal = { 24, 24 }
+local tileSize = { 32, 32 }
+
+local tileset = gfx.imagetable.new("sprites/tileset")
 
 class("Game").extends()
 
 function Game:init()
 
-    introScreen = Screen(self, gfx.kColorWhite)
-    test = ScreenSprite("player", 200, 120)
-    introScreen:add(test)
 
-    gameScreen = Screen(self)
-    other = ScreenSprite("block", 200, 120)
-    gameScreen:add(other)
+    battleScreen = BattleScreen(self)
 
-    self.screens = { intro = introScreen, game = gameScreen }
 
-    self:changeCurrentScreen("intro")
+    self.screens = { battle = battleScreen }
+
+    self:changeCurrentScreen("battle")
 
 end
 
@@ -72,22 +73,29 @@ function Screen:add(sprite)
     sprite.screen = self
 end
 
+function Screen:remove(sprite)
+    table.remove(self.sprites, table.indexOfElement(sprite))
+    sprite:remove()
+    sprite.screen = nil
+end
+
 function Screen:update()
-    if (pd.buttonJustPressed(pd.kButtonA)) then
-        self.game:changeCurrentScreen("game")
-    end
 
 end
 
 class("ScreenSprite").extends(gfx.sprite)
 
-function ScreenSprite:init(imagePath, x, y)
+function ScreenSprite:init(imagePath)
 
-    imagePath = "sprites/" .. imagePath
+    if pcall(function() imagePath = "sprites/" .. imagePath end) then
+        image = gfx.image.new(imagePath)
+    else
+        image = imagePath
+    end
+
+
     ScreenSprite.super.init(self)
-    image = gfx.image.new(imagePath)
     self:setImage(image)
-    self:moveTo(x, y)
 
     self.screen = nil
 
@@ -95,7 +103,63 @@ end
 
 function ScreenSprite:update()
 
-    if (self.screen.active) then
+    if (self.screen == nill or self.screen.active) then
         ScreenSprite.super.update(self)
     end
+end
+
+class("Tile").extends(ScreenSprite)
+
+function Tile:init(tileX, tileY, x, y)
+
+    image = tileset:getImage(tileX, tileY)
+    Tile.super.init(self, image)
+    self.mapX = x
+    self.mapY = y
+    self:setCenter(0, 0)
+    self:moveTo(mapDecal[1] + tileSize[1] * x, mapDecal[2] + tileSize[2] * y)
+    print(x, y)
+    self:setZIndex(10)
+
+end
+
+-----------SPECIFICS------------------------
+
+class("BattleScreen").extends(Screen)
+
+function BattleScreen:init(game)
+    BattleScreen.super.init(self, game, gfx.kColorWhite)
+
+    -- BG --
+
+    bg = ScreenSprite("bg")
+    bg:setCenter(0, 0)
+    bg:moveTo(0, 0)
+    bg:setZIndex(-100)
+    self:add(bg)
+
+    -- FG --
+    fg = ScreenSprite("fg")
+    fg:setCenter(0, 0)
+    fg:moveTo(0, 0)
+    fg:setZIndex(100)
+    self:add(fg)
+
+    self.battleTiles = {}
+    self:InitializeField()
+
+end
+
+function BattleScreen:InitializeField()
+
+    for x = 1, mapSize[1], 1 do
+        for y = 1, mapSize[2], 1 do
+            if math.random(10) == 1 then
+                tile = Tile(1, 1, x, y)
+                self:add(tile)
+                table.insert(self.battleTiles, tile)
+            end
+        end
+    end
+
 end
