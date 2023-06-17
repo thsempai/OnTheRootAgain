8db1b1fbb5f93897788ca83ca0ae9e11fd007ecd
Thomas Stassin <thomas.stassin@gsmproductions.com>
2023-06-17 09:56:46 +0200

    enhance sound system

--- source/sound.lua
+++ source/sound.lua
@@ -4,11 +4,16 @@ local pd <const> = playdate
 class("SoundBox").extends()
 
 function SoundBox:init()
-    self.hurtSounds = convert({ "hurt" })
+    self.hurtSounds = convert({ "hurt1", "hurt2", "hurt3", "hurt4" })
+    self.previousSound = ""
+    self.previousIndex = 0
 end
 
 function SoundBox:hurt()
     index = math.random(1, #self.hurtSounds)
+    while self.previousSound == "hurt" and self.previousIndex == index do
+        index = math.random(1, #self.hurtSounds)
+    end
     self.hurtSounds[index]:play()
     print("Here")
 end
